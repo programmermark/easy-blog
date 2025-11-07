562bcea45aa8e0dc4d347359385cf1b4ce70353e
爱编程的Mark <1126765590@qq.com>
2025-11-07 14:12:43 +0800

    fix(admin): set webpack alias for src

--- easy-blog-admin/next.config.js
+++ easy-blog-admin/next.config.js
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "@": path.resolve(__dirname, "src"),
+    };
+    return config;
+  },
+};
 
 module.exports = nextConfig;
