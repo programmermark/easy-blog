b52fef575c4079ad8e93467f8d479715602b8a3c
爱编程的Mark <1126765590@qq.com>
2025-11-07 13:49:18 +0800

    refactor(admin): align build config with frontend (pnpm/bundler)

--- easy-blog-admin/next.config.js
+++ easy-blog-admin/next.config.js
@@ -1,22 +1,5 @@
 /** @type {import('next').NextConfig} */
-const path = require("path");
-const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
-
-const nextConfig = {
-  webpack: (config) => {
-    const srcPath = path.resolve(__dirname, "src");
-    config.resolve.alias = {
-      ...(config.resolve.alias || {}),
-      "@": srcPath,
-      "@/": `${srcPath}/`,
-    };
-    config.resolve.modules = [srcPath, ...(config.resolve.modules || [])];
-    config.resolve.plugins = [
-      ...(config.resolve.plugins || []),
-      new TsconfigPathsPlugin({ configFile: path.resolve(__dirname, "tsconfig.json") }),
-    ];
-    return config;
-  },
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
 module.exports = nextConfig;
